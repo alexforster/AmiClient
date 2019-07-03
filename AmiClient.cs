013133eaa6096c9aa2b1048ef7bc46e51e2d0f61
Denis Karpovskiy <karpovsky.d@roskvartal.ru>
2019-07-03 12:03:05 +0300

    Avoid an infinity loop in LineObserver()
    Currently if somehow terminator bytes haven't been found
    in the buffer, the function just continues the outer loop,
    but it can't acquire more because the buffer isn't empty.
    So we must first try to acquire bytes and if we get nothing
    then we will just replace the buffer with an empty byte array.
    Or else we will have a chance to find terminator bytes this time

--- AmiClient.cs
+++ AmiClient.cs
@@ -142,13 +142,10 @@ namespace Ami
 			{
 				if(!this.readBuffer.Any())
 				{
-					var bytes = new Byte[4096];
-					var nrBytes = this.stream.Read(bytes, 0, bytes.Length);
-					if(nrBytes == 0)
+					if (GetBytes() == 0)
 					{
 						break;
 					}
-					this.readBuffer = this.readBuffer.Append(bytes.Slice(0, nrBytes));
 				}
 
 				while(this.readBuffer.Any())
@@ -156,14 +153,30 @@ namespace Ami
 					var crlfPos = this.readBuffer.Find(AmiMessage.TerminatorBytes, 0, this.readBuffer.Length);
 					if(crlfPos == -1)
 					{
-						goto CONTINUE;
+						if (this.GetBytes() == 0)
+						{
+							this.readBuffer = new byte[0];
+						}
+						break;
 					}
 					var line = this.readBuffer.Slice(0, crlfPos + AmiMessage.TerminatorBytes.Length);
 					this.readBuffer = this.readBuffer.Slice(crlfPos + AmiMessage.TerminatorBytes.Length);
 					yield return line;
 				}
-				CONTINUE: ;
 			}
+		}
+
+		private Int32 GetBytes()
+		{
+			var bytes = new Byte[4096];
+			var nrBytes = this.stream.Read(bytes, 0, bytes.Length);
+
+			if (nrBytes != 0)
+			{
+				this.readBuffer = this.readBuffer.Append(bytes.Slice(0, nrBytes));
+			}
+
+			return nrBytes;
 		}
 
 		private Boolean processing;
@@ -190,8 +203,8 @@ namespace Ami
 					var payload = new Byte[0];
 
 					await lineObserver
-					     .TakeUntil(line => line.SequenceEqual(AmiMessage.TerminatorBytes))
-					     .Do(line => payload = payload.Append(line));
+						.TakeUntil(line => line.SequenceEqual(AmiMessage.TerminatorBytes))
+						.Do(line => payload = payload.Append(line));
 
 					this.DataReceived?.Invoke(this, new DataEventArgs(payload));
 
